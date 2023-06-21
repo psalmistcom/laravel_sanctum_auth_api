4672f5cfc38f4160cfb7efb1288f14db0b7dc6e2
psalmistcom <dadewale9@gmail.com>
2023-06-21 16:11:45 +0100

    login endpoint

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,19 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginUserRequest;
 use App\Http\Requests\StoreUserRequest;
 use App\Models\User;
 use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     use HttpResponses;
 
-    public function login()
+    public function login(LoginUserRequest $request)
     {
-        return response()->json('This is the login method');
+
+        $request->validated($request->all());
+
+        $data = Auth::attempt($request->only(['email', 'password']));
+        if (!$data) {
+            return $this->error('', 'Credential do not match', 400);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken('API token of ' . $user->name)->plainTextToken
+        ]);
     }
 
     public function register(StoreUserRequest $request)
