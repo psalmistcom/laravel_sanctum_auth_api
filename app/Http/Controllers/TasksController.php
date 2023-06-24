d9746be9221e417e004752dd3589857e24cb92a9
psalmistcom <dadewale9@gmail.com>
2023-06-24 10:43:20 +0100

    get all tasks and create Task resources

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TasksController extends Controller
@@ -13,7 +14,8 @@ class TasksController extends Controller
      */
     public function index()
     {
-        return response()->json('Index');
+
+        return Task::all();
     }
 
     /**
