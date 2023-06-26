748ae392cbdccc65037600d49d6ef8edff7d79d4
psalmistcom <dadewale9@gmail.com>
2023-06-26 08:48:22 +0100

    fix

--- database/migrations/2023_06_19_190425_create_tasks_table.php
+++ database/migrations/2023_06_19_190425_create_tasks_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->string('name');
-            $table->string('description');
+            $table->text('description');
             $table->string('priority')->default('medium');
             $table->foreign('user_id')
                 ->references('id')
